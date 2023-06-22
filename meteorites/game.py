caa975d4e5da8966fb96950348fc4f95b575a690
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-06-23 02:30:37 +0300

    added spuds for rest of setup functions

--- meteorites/game.py
+++ meteorites/game.py
@@ -6,14 +6,25 @@ class Meteorites:
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("Meteorites")
+        self.clock = pygame.time.Clock()
         
     def main_loop(self):
         while True:
-            self._input_handle()
+            self.__input_handle()
+            self.__game_logic()
+            self.__update_screen
             
-    def _input_handle(self):
+    def __input_handle(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 pygame.quit()
                 exit()
+                
+    def __game_logic(self):
+        pass
+    
+    def __update_screen(self):
+        pygame.display.update()
+        self.clock.tick(60)
+        
                 
