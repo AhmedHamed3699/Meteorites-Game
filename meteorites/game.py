67fd3e4564780b263fa8473f509b972df77d5395
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-06-24 01:34:05 +0300

    created start message & backgrounds for modes

--- meteorites/game.py
+++ meteorites/game.py
@@ -1,7 +1,8 @@
 import pygame
 from sys import exit
 import data
-import utils
+from utils import load_sprite
+import models
 
 class Meteorites:
     def __init__(self):
@@ -9,31 +10,51 @@ class Meteorites:
         self.screen = pygame.display.set_mode((data.WIN_WIDTH, data.WIN_HIGHT))
         pygame.display.set_caption("Meteorites")
         self.clock = pygame.time.Clock()
-        self.mode = data.Mode.STOP
+        self.mode = data.Mode.START
+        self.background = pygame.surface.Surface((data.WIN_WIDTH, data.WIN_HIGHT))
+        self.background.fill((58, 46, 63))
+        self.font = pygame.font.Font("assets/Font/kenvector_future.ttf", 28)
+        self.start_text = self.font.render("Press SPACE to start", True, (255, 255, 255))
+        self.start_text_rect = self.start_text.get_rect(center = (data.WIN_WIDTH//2, data.WIN_HIGHT//2))
         self.__load_sprites()
         
     def __load_sprites(self):
-        self.background = utils.load_sprite("Backgrounds/blue")
-        self.background = pygame.transform.scale(self.background, (data.WIN_WIDTH, data.WIN_HIGHT))
+        pass
         
     def main_loop(self):
         while True:
             self.__input_handle()
-            self.__game_logic()
+            
+            if self.mode == data.Mode.RUNNING:
+                self.__game_logic()
+            elif self.mode == data.Mode.OVER:
+                self.__over_menu()
+            
             self.__update_screen()
+                
             
     def __input_handle(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-                
+            if self.mode == data.Mode.START:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    self.mode = data.Mode.RUNNING
+    
     def __game_logic(self):
-        pass
+        self.background.fill((0, 0, 0))
+    
+    def __over_menu(self):
+        self.background.fill((118, 38, 0))
     
     def __update_screen(self):
         self.screen.blit(self.background, (0,0))
+        if self.mode == data.Mode.START:
+            self.screen.blit(self.start_text, self.start_text_rect)
         pygame.display.update()
-        self.clock.tick(60)
+        self.clock.tick(data.FRAME_RATE)
+        
+    
         
                 
