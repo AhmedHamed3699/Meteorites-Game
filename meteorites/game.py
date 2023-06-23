8a36af0198c2738fef0b26767f876e8fe0a484e6
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-06-23 05:07:51 +0300

    added mode

--- meteorites/game.py
+++ meteorites/game.py
@@ -1,18 +1,20 @@
 import pygame
 from sys import exit
+import data
 
 class Meteorites:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((data.WIN_WIDTH, data.WIN_HIGHT))
         pygame.display.set_caption("Meteorites")
         self.clock = pygame.time.Clock()
+        self.mode = data.Mode.STOP
         
     def main_loop(self):
         while True:
             self.__input_handle()
             self.__game_logic()
-            self.__update_screen
+            self.__update_screen()
             
     def __input_handle(self):
         for event in pygame.event.get():
