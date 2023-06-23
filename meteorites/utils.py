07374801524dd3eb02a8fa2926501694a169e95e
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-06-23 05:28:49 +0300

    modified path to sprites

--- meteorites/utils.py
+++ meteorites/utils.py
@@ -1,7 +1,7 @@
 from pygame.image import load
 
 def load_sprite(name: str, alpha=False):     
-    sprite = load(f"../assets/sprite/{name}.png")
+    sprite = load(f"assets/{name}.png")
     
     if alpha: sprite.convert()
     else: sprite.convert_alpha()
