3a220914a5896e54f7ad1ac8134b56af382c6633
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-06-24 01:32:06 +0300

    added module for models

---
+++ meteorites/models.py
@@ -0,0 +1,23 @@
+from pygame import sprite, transform
+import data
+from utils import load_sprite
+
+class Player(sprite.Sprite):
+    pass
+
+class Meteorite(sprite.Sprite):
+    pass
+
+class Bullet(sprite.Sprite):
+    pass
+
+class Explosion(sprite.Sprite):
+    pass
+
+class MeteoriteExplosion(sprite.Sprite):
+    pass
+
+class PlayerExplosion(sprite.Sprite):
+    pass
+
+
