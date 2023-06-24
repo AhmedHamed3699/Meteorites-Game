21dc39005d53a7fb44245fcd2f6b45971c7e0357
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-06-24 04:14:38 +0300

    Added some related player constants

--- meteorites/data.py
+++ meteorites/data.py
@@ -1,9 +1,14 @@
 from enum import Enum
+from pygame.math import Vector2
 
 WIN_WIDTH = 800
 WIN_HIGHT = 600
 
 FRAME_RATE = 60
+
+# player
+DEACCELERATION = 0.95
+ROTATION_SPEED = 4
 
 class Mode(Enum):
     START = 0
