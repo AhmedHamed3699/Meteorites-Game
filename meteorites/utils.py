e6618b52ad22dda821fcb166b99beaf5336a5873
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-07-05 04:39:50 +0300

    Added a function for bullet collision & Enhances the quality of collision

--- meteorites/utils.py
+++ meteorites/utils.py
@@ -1,4 +1,5 @@
 from pygame.image import load
+from pygame.sprite import collide_mask
 
 def load_sprite(name: str, alpha=False):     
     sprite = load(f"assets/{name}.png")
@@ -10,6 +11,13 @@ def load_sprite(name: str, alpha=False):
 
 def collision_check(sprite, group):
     for target in group:
-        if sprite.collide(target):
+        if collide_mask(sprite, target):
             return True
     return False
+
+def bullet_collision_check(bullets, obstacles):
+    for bullet in bullets:
+        for target in obstacles:
+            if collide_mask(bullet, target):
+                bullet.kill()
+                target.kill()
