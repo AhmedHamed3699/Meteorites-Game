26c3d4032001860e74a1470ac7f4628ece3fb544
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-07-05 04:43:43 +0300

    Created Bullets & Edited some stuff

--- meteorites/models.py
+++ meteorites/models.py
@@ -12,20 +12,30 @@ class AdvavcedSprite(sprite.Sprite):
         self.vel = math.Vector2(vel)
         self.image = load_sprite("Sprite/" + name, True)
         self.image = transform.scale_by(self.image, scale)
+        self.mask = pygame.mask.from_surface(self.image)
         self.radius = self.image.get_width()//2
+        self.rect = self.image.get_rect(center = self.pos)
         
-    def move(self):
+    def _move(self):
         self.pos += self.vel
+        self.rect.center = round(self.pos.x), round(self.pos.y)
         
-    def draw(self, surface):
-        surface.blit(self.image, self.pos - math.Vector2(self.radius))
+    def _draw(self, surface):
+        surface.blit(self.image, self.rect)
         
-    def collide(self, other):
-        return (self.pos - other.pos).length() < (self.radius + other.radius)
+    def _destroy(self):
+        if (self.pos.x > data.WIN_WIDTH + data.OFF_SCREEN or self.pos.x < -data.OFF_SCREEN) or ( 
+            self.pos.y > data.WIN_HIGHT + data.OFF_SCREEN or self.pos.y < -data.OFF_SCREEN):
+            self.kill()
+            
+    def update(self, surface):
+        self._draw(surface)
+        self._move()
+        self._destroy()
     
     
 class Player(AdvavcedSprite):
-    def __init__(self, pos, name="Player_Ships/playerShip3_red", scale=0.75):
+    def __init__(self, pos, name="Player_Ships/playerShip3_red", scale=0.65):
         super().__init__(name=name, scale=scale, pos=pos, vel=math.Vector2(0, 0))
         self.dir = data.UP * data.PLAYER_SPEED
     
@@ -58,13 +68,22 @@ class Player(AdvavcedSprite):
             self.pos.y = data.WIN_HIGHT
             
         self.vel *= data.DEACCELERATION
-        super().move()
+        super()._move()
+        
+        
+    def shoot(self):
+        bullet_vel = self.dir * data.BULLET_SPEED + self.vel
+        bullet_pos = self.pos + self.dir.normalize() * self.radius
+        bullet = Bullet(bullet_pos, bullet_vel)
+        return bullet
         
        
     def __draw(self, surface):
         rotated_surf = transform.rotozoom(self.image, self.dir.angle_to(math.Vector2(0, -1)), 1.0)
-        new_radius = rotated_surf.get_width()//2
-        surface.blit(rotated_surf, self.pos - math.Vector2(new_radius))
+        self.radius = rotated_surf.get_width()//2
+        self.mask = pygame.mask.from_surface(rotated_surf)
+        self.rect = rotated_surf.get_rect(center = self.pos)
+        surface.blit(rotated_surf, self.rect)
         
     def update(self, surface):
         self.__draw(surface)
@@ -80,7 +99,7 @@ class Meteorite(AdvavcedSprite):
         y = (randint(-data.OFF_SCREEN, data.WIN_WIDTH + data.OFF_SCREEN), choice([-data.OFF_SCREEN, data.WIN_HIGHT + data.OFF_SCREEN]))
         c = choice([x,y])
         pos = (c)
-        vel = data.UP * randint(2, 5)
+        vel = data.UP * randint(2, 4)
         
         if pos[0] < 0:
             vel.rotate_ip(randint(50, 130))
@@ -92,21 +111,24 @@ class Meteorite(AdvavcedSprite):
             vel.rotate_ip(randint(120, 240))
             
         super().__init__(name=name, scale=scale, pos=pos, vel=vel)
-   
-        
-    def __destroy(self):
-        if (self.pos.x > data.WIN_WIDTH + 2*data.OFF_SCREEN or self.pos.x < -data.OFF_SCREEN) or ( 
-            self.pos.y > data.WIN_HIGHT + 2*data.OFF_SCREEN or self.pos.y < -2*data.OFF_SCREEN):
-            self.kill()
-            
-    def update(self, surface):
-        self.draw(surface)
-        self.move()
-        self.__destroy()
         
 
 class Bullet(AdvavcedSprite):
-    pass
+    def __init__(self, pos, vel):
+        super().__init__(name="Lasers/laserRed07", scale=0.4, pos=pos, vel=vel)
+    
+    def __draw(self, surface):   
+        rotated_surf = transform.rotozoom(self.image, self.vel.angle_to(math.Vector2(0, -1)), 1.0)
+        self.radius = rotated_surf.get_width()//2
+        self.mask = pygame.mask.from_surface(rotated_surf)
+        self.rect = rotated_surf.get_rect(center = self.pos)
+        surface.blit(rotated_surf, self.rect)
+    
+    def update(self, surface):
+        self.__draw(surface)
+        self._move()
+        self._destroy()
+        
 
 class Explosion(sprite.Sprite):
     pass
