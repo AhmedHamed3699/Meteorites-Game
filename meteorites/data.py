c447ec30bf1a24f30097d787f3804d288ec8dcbf
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-07-10 01:06:57 +0300

    added data for sounds & updated other data

--- meteorites/data.py
+++ meteorites/data.py
@@ -1,15 +1,15 @@
 from enum import Enum
 from pygame.math import Vector2
 
-WIN_WIDTH = 800
-WIN_HIGHT = 600
+WIN_WIDTH = 900
+WIN_HIGHT = 700
 
 FRAME_RATE = 80
 
 # player
 PLAYER_SCALE = 0.65
 DEACCELERATION = 0.95
-ROTATION_SPEED = 3
+ROTATION_SPEED = 4
 PLAYER_SPEED = 0.4
 UP = Vector2(0, -1)
 
@@ -22,6 +22,7 @@ OFF_SCREEN_RANGE = 50
 
 # bullets
 BULLET_SPEED = 20
+SOUND_VOLUME = 0.1
 
 class Mode(Enum):
     START = 0
